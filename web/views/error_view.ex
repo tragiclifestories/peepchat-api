f1bce9fa81cbcd24402de0bc0c1fb1d4b1d86460
James Turley <jamesturley1905@googlemail.com>
2016-05-14 10:44:12 +0100

    fix tests

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,14 +2,9 @@ defmodule Peepchat.ErrorView do
   use Peepchat.Web, :view
   use JaSerializer.PhoenixView
 
-  def render("404.json", _assigns) do
-    %{title: "not found", code: 404}
-    |> JaSerializer.ErrorSerializer.format
-  end
-
   def render("401.json", _assigns) do
-    %{title: "unauthorized", code: 401}
-    |> JaSerializer.ErrorSerializer.format
+    %{title: "Unauthorized", code: 401}
+    |> JaSerializer.ErrorSerializer.format 
   end
 
   def render("403.json", _assigns) do
@@ -17,13 +12,18 @@ defmodule Peepchat.ErrorView do
     |> JaSerializer.ErrorSerializer.format
   end
 
-  def render("500.json", _assigns) do
-    %{errors: %{detail: "Server internal error"}}
+  def render("404.json", _assigns) do
+    %{title: "Not Found", code: 404}
     |> JaSerializer.ErrorSerializer.format
   end
 
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
+  def render("500.json", _assigns) do
+    %{title: "Internal Server Error", code: 500}
+    |> JaSerializer.ErrorSerializer.format 
+  end
+
+  # # In case no render clause matches or no
+  # # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
     render "500.json", assigns
   end
